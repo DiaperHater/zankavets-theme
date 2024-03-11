1534c82a1aa02112fa686d0b9418c00fa492a85f
Valery Pronin <pownmymail@gmail.com>
2024-03-11 20:42:42 +0300

    fix: Prose images  on the frontend doesn't have same width as in editor

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 define('DEBUG', false);
 
 if (!defined('S_VERSION')) {
-  define('S_VERSION', DEBUG ? microtime() : '1.0.3');
+  define('S_VERSION', DEBUG ? microtime() : '1.0.4');
 }
 
 add_theme_support('title-tag');
