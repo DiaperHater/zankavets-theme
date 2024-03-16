42d08f77f7df31f14d064accc29654b705b9607f
Valery Pronin <pownmymail@gmail.com>
2024-03-16 23:37:10 +0300

    feat: Add one more telnum to Contact page

--- page-contacts.php
+++ page-contacts.php
@@ -5,21 +5,30 @@
     <div class="box">
       <div class="lg:flex lg:justify-between lg:items-start">
         <!-- Heading -->
-        <h2 class="heading text-neutral-900 mb-12">
+        <h2 class="mb-12 heading text-neutral-900">
           Contacts
         </h2>
         <!-- TelAndEmail -->
         <div class="mb-12">
-          <p class="text-3xl text-right text-neutral-900 mb-2">
-            Tel: <a href="<?= get_field('telnum')['url'] ?>" class="hover:text-yellow-500 duration-300 hover:underline"><?= get_field('telnum')['title'] ?></a>
+          <p class="mb-2 text-3xl text-right text-neutral-900">
+            Tel: <a href="<?= get_field('telnum')['url'] ?>" class="duration-300 hover:text-yellow-500 hover:underline"><?= get_field('telnum')['title'] ?></a>
           </p>
+          <?php
+          if (get_field('telnum_2')['url']) {
+          ?>
+            <p class="mb-2 text-3xl text-right text-neutral-900">
+              Tel: <a href="<?= get_field('telnum_2')['url'] ?>" class="duration-300 hover:text-yellow-500 hover:underline"><?= get_field('telnum_2')['title'] ?></a>
+            </p>
+          <?php
+          }
+          ?>
           <p class="text-3xl text-right text-neutral-900">
-            Email: <a href="mailto:<?= get_field('email') ?>" class="hover:text-yellow-500 duration-300 hover:underline"><?= get_field('email') ?></a>
+            Email: <a href="mailto:<?= get_field('email') ?>" class="duration-300 hover:text-yellow-500 hover:underline"><?= get_field('email') ?></a>
           </p>
         </div>
         <!-- Socials -->
         <div class="">
-          <p class="text-xl font-semibold text-right mb-4 text-neutral-900">
+          <p class="mb-4 text-xl font-semibold text-right text-neutral-900">
             Open to connection:
           </p>
           <div class="flex justify-end gap-3 [&_svg]:text-neutral-900 [&_svg]:w-8 [&_svg]:h-8">
