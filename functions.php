520c4dd65ac977fdda190dc78fc0a59a4838b29d
Valery Pronin <pownmymail@gmail.com>
2024-03-22 20:21:01 +0300

    Update S_VERSION

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 define('DEBUG', false);
 
 if (!defined('S_VERSION')) {
-  define('S_VERSION', DEBUG ? microtime() : '1.0.8');
+  define('S_VERSION', DEBUG ? microtime() : '1.0.9');
 }
 
 add_theme_support('title-tag');
