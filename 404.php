346d7eb3f978cfb1e93e3fb5edda5db33e69e044
Valery Pronin <pownmymail@gmail.com>
2024-03-22 20:19:19 +0300

    feat: Add 404 page

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php get_header() ?>
+
+
+<section class="bg-white section">
+  <div class="wrapper">
+    <div class="box">
+      <!-- component -->
+      <madivin class="h-[80vh] w-full flex flex-col justify-center items-center">
+        <h1 class="font-extrabold tracking-widest text-neutral-600 text-9xl">
+          404
+        </h1>
+        <div class="absolute px-2 text-sm bg-yellow-300 rounded rotate-12 text-neutral-900">
+          Page Not Found
+        </div>
+        <a href="/" class="mt-12 button">
+          Go Home
+        </a>
+    </div>
+  </div>
+  </div>
+</section>
+
+
+
+
+<?php get_footer() ?>
